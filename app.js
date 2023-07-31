eafa055b89bfca6cea7c3e1a8a8b22d8cdf73c91
Emir Sorroza <lucasemirsorroza@gmail.com>
2023-07-31 07:26:49 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log("hola mundo");
+
+let usuario = "Pepe";
+usuario = "Juan";
+usuario = 5;
+
+console.log(usuario);
+
+// let respuestaUsuario;
+// respuestaUsuario = prompt("cual es tu nombre?");
+
+// console.log(respuestaUsuario);
+
+document.write("hola");
+
+let respuestaUsuario;
+// respuestaUsuario = prompt("ingrese su nombre");
+
+// alert("el nombre que ingresaste es : " + respuestaUsuario);
+console.log("el nombre que ingresaste es : " + respuestaUsuario);
+
+let hola;
+
+console.log(2 + {});
